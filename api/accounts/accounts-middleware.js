8348f4afa28778939d6adc08f0db1186d52529ac
Matthew <mattcasey879@gmail.com>
2021-09-16 15:31:14 -0500

    finished validation

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,34 +1,55 @@
-const Acc = require("./accounts-model")
-
-
-
-
+const Acc = require("./accounts-model");
+const db = require("../../data/db-config")
 
 exports.checkAccountPayload = (req, res, next) => {
-  const { name, budget } = req.body
-  if (!name || !budget) {
-    next({ status: 400, message: "name and budget are required" })
-  } else if (typeof name !== 'string') {
-    next({ status: 400, message: "name must be a string"})
+  const { name, budget } = req.body;
+  const error = { status: 400 };
+  if (name === undefined || budget === undefined) {
+    error.message = "name and budget are required";
+    next(error);
+  } else if (typeof name !== "string") {
+    error.message = "name of account must be a string";
+    next(error);
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100";
+    next(error);
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    error.message = "budget of account must be a number";
+    next(error);
+  } else if (budget < 0 || budget > 100000) {
+    error.message = "budget of account is too large or too small";
   } else {
-    next()
+    next();
   }
-} 
+};
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+
+      if (existing) {
+        next({ status: 400, message: "that name is taken"})
+      } else {
+        next()
+      }
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.checkAccountId = async (req, res, next) => {
   try {
-    const account = await Acc.getById(req.params.id)
+    const account = await Acc.getById(req.params.id);
 
     if (account) {
-      res.account = account
+      res.account = account;
       next();
     } else {
-      next({ status: 404, message: "account not found"})
+      next({ status: 404, message: "account not found" });
     }
-  } catch (err) { next(e) }
-    
-}
+  } catch (err) {
+    next(e);
+  }
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { checkAccountId, checkAccountPayload } = require("./accounts-middleware")
+const { checkAccountId, checkAccountPayload, checkAccountNameUnique } = require("./accounts-middleware")
 const Acc = require("./accounts-model");
 
 router.get('/',async (req, res, next) => {
@@ -18,22 +18,16 @@ router.get('/:id', checkAccountId, async (req, res, next) => {
   res.json(res.account)
 })
 
-router.post('/', checkAccountPayload, (req, res, next) => {
-  const newAcc = req.body
-  // using async await 
-  //    try {
-  //   const data = await Acc.create(newAcc)
-  //   res.status(200).json(data)
-  // } catch (e) {
-  //   next(e)
-  // }
-  // ending of using async await
-  Acc.create(newAcc)
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  Acc.create({
+    name: req.body.name.trim(),
+    budget: req.body.budget
+  })
   .then(acc => res.status(201).json(acc))
   .catch(next)
 })
 
-router.put('/:id', checkAccountId, checkAccountPayload, async(req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, checkAccountNameUnique, async(req, res, next) => {
   const acc = req.body
   try {
     const newAcc = await Acc.updateById(req.params.id, acc)
